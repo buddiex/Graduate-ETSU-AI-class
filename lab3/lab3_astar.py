f6bbca82f1ebeb17f607250ca6c408a43087a337
osayamen <mailbuddie@gmail.com>
2019-02-08 05:01:58 -0500

    Lab 3 A*completed

--- lab3/lab3_astar.py
+++ lab3/lab3_astar.py
@@ -10,7 +10,7 @@ import numpy as np
 # Goal: Solve the 8-puzzle using A*Search
 # ==============================================================================
 
-#===============================================================================
+# ===============================================================================
 # Method: method_timing
 #  Purpose: A timing function that wraps the called method with timing code.
 #     Uses: time.time(), used to determine the time before an after a call to
@@ -20,67 +20,59 @@ def method_timing(func):
         t1 = time.time()
         res = func(*arg)
         t2 = time.time()
-        print ('%s took %0.3f ms' % (func, (t2-t1)*1000.0))
-        return [res,(t2-t1)*1000.0]
+        print('%s took %0.3f ms for %s' % (func.__name__, (t2 - t1) * 1000.0, arg[1]))
+        return [res, (t2 - t1) * 1000.0]
+
     return wrapper
 
-#===============================================================================
+
+# ===============================================================================
 # Class: PriorityQueue
 #  Purpose: A simplified PriorityQueue
 class PriorityQueue:
 
+    def __init__(self):
+        self.queue = []
 
-	def __init__(self):
-		self.queue = []
+    def put(self, item, priority):
+        node = [item, priority]
+        self.queue.append(node)
+        self.queue.sort(key=itemgetter(1))
 
+    def get(self):
+        if len(self.queue) == 0:
+            return None
+        node = self.queue.pop(0)
+        return node[0]
 
-	def put(self, item, priority):
-		node = [item,priority]
-		self.queue.append(node)
-		self.queue.sort(key=itemgetter(1))
-
-
-	def get(self):
-		if len(self.queue) == 0:
-			return None
-		node = self.queue.pop(0)
-		return node[0]
-
-
-	def empty(self):
-		return len(self.queue) == 0
+    def empty(self):
+        return len(self.queue) == 0
 
 
-#===============================================================================
+# ===============================================================================
 # Class: Board
 #  Purpose: Represents an 8-puzzle game Board
 class Board:
 
-
-    def __init__(self):
+    def __init__(self, board=None):
         '''
             Default Constructor
         '''
-        self.board = random.sample(range(0,9),9)
-        while not self.solvable():
-            self.board = random.sample(range(0,9),9)
-
-
-    def __init__(self, board):
-        '''
-            Default Constructor
-        '''
-        self.board = board
-        if not self.solvable():
-            raise Exception("This Board is not Solvable.")
-
+        if board:
+            self.board = board
+            if not self.solvable():
+                raise Exception("This Board is not Solvable.")
+            self.board = board
+        else:
+            self.board = random.sample(range(0, 9), 9)
+            while not self.solvable():
+                self.board = random.sample(range(0, 9), 9)
 
     def solvable(self):
         '''
             Returns True if the board is solvable (inversions must be even)
         '''
-        return self.inversions()%2 != 1
-
+        return self.inversions() % 2 != 1
 
     def inversions(self):
         '''
@@ -90,85 +82,70 @@ class Board:
         numbers = deepcopy(self.board)
         numbers.remove(0)
 
-        inv = len([ (i,j) for i in range(len(numbers)) for j in range(i+1,len(numbers)) if(numbers[j]>numbers[i]) ])
+        inv = len(
+            [(i, j) for i in range(len(numbers)) for j in range(i + 1, len(numbers)) if (numbers[j] > numbers[i])])
 
         return inv
 
-
-    def to_s(self):
-        '''
-            Returns a string representation of the Board
-        '''
-        shaped = np.reshape(np.array(self.board),(3,3))
-        string = '\n'.join(' '.join(map(str, x)) for x in shaped)
-        return string.replace("0"," ")
-
-
-    def to_s(self,state):
+    def to_s(self, state=None):
         '''
             Returns a string representation of the state passed
         '''
-        shaped = np.reshape(np.array(state),(3,3))
+        shaped = np.reshape(np.array(state or self.board), (3, 3))
         string = '\n'.join(' '.join(map(str, x)) for x in shaped)
-        return string.replace("0"," ")
+        return string.replace("0", " ")
 
 
-
-#===============================================================================
+# ===============================================================================
 # Class: Solver
 #  Purpose: Solves an 8-puzzle Board
 class Solver:
 
-
-    def __init__(self,board):
+    def __init__(self, board):
         '''
             Constructor: accepts a game board
         '''
         self.board = board
-        self.solution = [1,2,3,4,5,6,7,8,0]
+        self.solution = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
-        self.rules = [[] for x in range(9)]
-        self.rules[0] = [1,3]
-        self.rules[1] = [0,2,4]
-        self.rules[2] = [1,5]
-        self.rules[3] = [0,4,6]
-        self.rules[4] = [1,3,5,7]
-        self.rules[5] = [2,4,8]
-        self.rules[6] = [3,7]
-        self.rules[7] = [4,6,8]
-        self.rules[8] = [5,7]
+        self.rules = []
+        self.rules.append([1, 3])
+        self.rules.append([0, 2, 4])
+        self.rules.append([1, 5])
+        self.rules.append([0, 4, 6])
+        self.rules.append([1, 3, 5, 7])
+        self.rules.append([2, 4, 8])
+        self.rules.append([3, 7])
+        self.rules.append([4, 6, 8])
+        self.rules.append([5, 7])
 
-        
-    def hamming(self,state):
+    def hamming(self, state):
         '''
             Hamming heuristic: Determine the wrong positions for a given state
         '''
-        errors = len([ x for x in range(9) if state[x] != self.solution[x] ])
+        errors = len([x for x in range(9) if state[x] != self.solution[x]])
         return errors
 
-
-    def manhattan(self,state):
+    def manhattan(self, state):
         '''
             Manhattan Heuristic: Determine the Sum of the Manhattan distances for a given state
         '''
         man_dist = 0
-        for i,item in enumerate(state):
-            curr_row,curr_col = int(i/ 3) , i % 3
-            goal_row,goal_col = int(item /3),item % 3
-            man_dist += abs(curr_row-goal_row) + abs(curr_col - goal_col)
+        for i, item in enumerate(state):
+            curr_row, curr_col = int(i / 3), i % 3
+            goal_row, goal_col = int(item / 3), item % 3
+            man_dist += abs(curr_row - goal_row) + abs(curr_col - goal_col)
 
         return man_dist
 
-
-    def swap(self,state,a,b):
+    def swap(self, state, a, b):
         '''
             Swap state a and state b
         '''
         state[a], state[b] = state[b], state[a]
         return state
 
-
-    def get_neighbors(self,state):
+    def get_neighbors(self, state):
         '''
             Returns a list of valid neighbors for the given position
               State is a list of nine numbers.
@@ -181,46 +158,44 @@ class Solver:
 
         for swap_index in self.rules[loc]:
             neighbor = deepcopy(state)
-            neighbor = self.swap(neighbor,loc,swap_index)
+            neighbor = self.swap(neighbor, loc, swap_index)
             neighbors.append(neighbor)
 
         return neighbors
 
-
     @method_timing
-    def astar_search(self):
+    def astar_search(self, heuristic_type="hamming"):
         '''
             Performs the A* search for the current board
         '''
-        steps = 0                       # Initialize the step counter
-        board = self.board.board        # Copy of the board
+        steps = 0  # Initialize the step counter
+        board = self.board.board  # Copy of the board
 
-        frontier = PriorityQueue()      # Create the frontier as a PriorityQueue
-        frontier.put(board,0)           # - And push the initial state to the frontier
+        frontier = PriorityQueue()  # Create the frontier as a PriorityQueue
+        frontier.put(board, 0)  # - And push the initial state to the frontier
 
-        came_from = {}                  # Use a dictionary to track the path
-        cost_so_far = {}                # Use a dictionary to track the path cost by state
+        came_from = {}  # Use a dictionary to track the path
+        cost_so_far = {}  # Use a dictionary to track the path cost by state
 
-        came_from[str(board)] = None    # Push the initial state and set it's parent as None
-        cost_so_far[str(board)] = 0     # The cost so far from the initial state is 0.
+        came_from[str(board)] = None  # Push the initial state and set it's parent as None
+        cost_so_far[str(board)] = 0  # The cost so far from the initial state is 0.
 
-        ## - - - -
-        # TODO: Complete the A* Algorithm Here.
-        #      Pseudocode for the algorithm is available on Wikipedia (which,
-        #       is actually a better pseudocode than the Best-First Search from the book).
-        #       https://en.wikipedia.org/wiki/A*_search_algorithm
-
-
-
-        # End code
-        ## - - - -
+        while frontier:
+            current_item = frontier.get()
+            if current_item == self.solution:
+                break
+            for num_swaps, neigbor in enumerate(self.get_neighbors(current_item)):
+                str_neigbor = str(neigbor)
+                str_current_item = str(current_item)
+                path_cost = cost_so_far[str(current_item)] + 1
+                if str_neigbor not in cost_so_far or path_cost < cost_so_far[str_neigbor]:
+                    cost_so_far[str_neigbor] = path_cost
+                    frontier.put(neigbor, path_cost+ getattr(self, heuristic_type)(neigbor))
+                    came_from[str_neigbor] = current_item
 
         node = self.solution
 
-        ## TODO: REMOVE THE FOLLOWING LINE OF CODE, OR THE SOLUTION WILL NOT BE DISPLAYED.
-        node = board
-
-        #-- Recreate the solution by walking back from the goal to the initial state
+        # -- Recreate the solution by walking back from the goal to the initial state
         solution = []
 
         while node != None:
@@ -228,21 +203,21 @@ class Solver:
             parent = came_from[str(node)]
             node = parent
 
-        #-- Reverse the solution so you move forward
+        # -- Reverse the solution so you move forward
         solution.reverse()
 
         for state in solution:
-            print(self.board.to_s(state),"\n")
+            print(self.board.to_s(state), "\n")
 
-        print ("\nTotal Moves:",str(len(solution)-1))
+        print("\nTotal Moves:", str(len(solution) - 1))
 
 
-
-#=====================
+# =====================
 # Main Algorithm
 
-tester1 = [8,1,3,4,0,2,7,6,5]
-tester2 = [1,8,0,4,3,2,5,7,6]
+tester1 = [8, 1, 3, 4, 0, 2, 7, 6, 5]
+tester2 = [1, 8, 0, 4, 3, 2, 5, 7, 6]
 board = Board(tester1)
 solver = Solver(board)
-solver.astar_search()
+solver.astar_search("hamming")
+solver.astar_search("manhattan")
--- lab3/lab3_ga.py
+++ lab3/lab3_ga.py
@@ -7,15 +7,17 @@ import numpy as np
 from matplotlib import pyplot as plot
 
 plot.ion()
-plot.figure(figsize=(9,5))
+plot.figure(figsize=(9, 5))
 
-CLUSTERS = 4            # Clusters to attempt
-DISPLAY_RATE = 100      # Show a graph at this rate
+CLUSTERS = 4  # Clusters to attempt
+DISPLAY_RATE = 100  # Show a graph at this rate
+
 
 class GeneticSearch:
     """
         Class: GeneticSearch
     """
+
     def __init__(self, filename, generations, population_size, mutation_rate):
         '''
             Initialize the GA by reading the points from the file and setting
@@ -30,31 +32,24 @@ class GeneticSearch:
         self.mutation_rate = mutation_rate
         self.values = []
 
-
-
     def read_file(self):
         '''
             Read points from the passed file in the format x,y
         '''
         self.points = np.genfromtxt(self.filename, delimiter=',')
 
-
-
     def initialize_population(self):
         '''
-            Create the initial population and find the fitness for each
-             individual.
+            Create the initial population and find the fitness for each individual.
         '''
         self.population = []
 
         for _ in range(self.population_size):
-            individual = [random.randint(1,CLUSTERS) for _ in range(self.chromosome_size)]
+            individual = [random.randint(1, CLUSTERS) for _ in range(self.chromosome_size)]
             fitness = self.fitnessfcn(individual)
-            self.population.append([individual,fitness])
+            self.population.append([individual, fitness])
 
-        self.population.sort(key=operator.itemgetter(1),reverse=True)
-
-
+        self.population.sort(key=operator.itemgetter(1), reverse=True)
 
     def sum_squared_distances(self, c_points, center):
         '''
@@ -64,17 +59,14 @@ class GeneticSearch:
         distances = 0
 
         for pt in c_points:
-            distances += (np.linalg.norm(pt-center)**2)
+                distances += (np.linalg.norm(pt - center) ** 2)
 
         return distances
 
-
-
-    def get_clusters_and_centers(self,individual):
+    def get_clusters_and_centers(self, individual):
         '''
             Using individual, it returns a dictionary of indexes for points in
-            each cluster, a list of points in each cluster, and centers for
-            each cluster.
+            each cluster, a list of points in each cluster, and centers for each cluster.
         '''
         clusters = {}
         c_points = {}
@@ -82,20 +74,17 @@ class GeneticSearch:
         c_vals = set(individual)
 
         for key in c_vals:
-            clusters[key] = [i for i,x in enumerate(individual) if x == key]
+            clusters[key] = [i for i, x in enumerate(individual) if x == key]
             c_points[key] = [self.points[x] for x in clusters[key]]
-            centers[key] = sum(c_points[key])/float(len(c_points[key]))
+            centers[key] = sum(c_points[key]) / float(len(c_points[key]))
 
-        return clusters,c_points,centers
-
-
+        return clusters, c_points, centers
 
     def fitnessfcn(self, individual):
         '''
-            The fitness function for the individual is the total
-             sum of squared distances for all clusters.
+            The fitness function for the individual is the total sum of squared distances for all clusters.
             The lowest value represents the best solution, so this is negated
-             to represent "maximum utility" idea of Genetic Algorithms
+            to represent "maximum utility" idea of Genetic Algorithms
         '''
         clusters, c_points, centers = self.get_clusters_and_centers(individual)
         sse = {}
@@ -104,18 +93,16 @@ class GeneticSearch:
         total_bss = 0
 
         for key in c_vals:
-            sse[key] = self.sum_squared_distances(c_points[key],centers[key])
+            sse[key] = self.sum_squared_distances(c_points[key], centers[key])
 
         return -sum(list(sse.values()))
 
-
-
-    def reproduce(self,parent1,parent2):
+    def reproduce(self, parent1, parent2):
         '''
             Reproduce using parent1 and parent2 and a crossover
              strategy.
         '''
-        crossover1 = random.randrange(0,self.chromosome_size)
+        crossover1 = random.randrange(0, self.chromosome_size)
 
         ''' Single point crossover:
               Pull bits 0..crossover1 from parentX.
@@ -124,13 +111,11 @@ class GeneticSearch:
         child1 = parent2[:crossover1] + parent1[crossover1:]
         child2 = parent1[:crossover1] + parent2[crossover1:]
 
-        value = [child1,child2]
+        value = [child1, child2]
 
         return value
 
-
-
-    def mutate(self,child):
+    def mutate(self, child):
         '''
             Mutation Strategy: Assign three random points to their closest
              cluster center
@@ -140,46 +125,42 @@ class GeneticSearch:
         distance2 = {}
         distance3 = {}
 
-        index1 = random.randint(0,self.chromosome_size-1)
-        index2 = random.randint(0,self.chromosome_size-1)
-        index3 = random.randint(0,self.chromosome_size-1)
+        index1 = random.randint(0, self.chromosome_size - 1)
+        index2 = random.randint(0, self.chromosome_size - 1)
+        index3 = random.randint(0, self.chromosome_size - 1)
 
         for key in set(child):
-            distance1[key] = np.linalg.norm(self.points[index1]-centers[key])**2
-            distance2[key] = np.linalg.norm(self.points[index2]-centers[key])**2
-            distance3[key] = np.linalg.norm(self.points[index3]-centers[key])**2
+            distance1[key] = np.linalg.norm(self.points[index1] - centers[key]) ** 2
+            distance2[key] = np.linalg.norm(self.points[index2] - centers[key]) ** 2
+            distance3[key] = np.linalg.norm(self.points[index3] - centers[key]) ** 2
 
-        child[index1] = [key for key,val in distance1.items() if val == min(distance1.values())][0]
-        child[index2] = [key for key,val in distance2.items() if val == min(distance2.values())][0]
-        child[index3] = [key for key,val in distance3.items() if val == min(distance3.values())][0]
+        child[index1] = [key for key, val in distance1.items() if val == min(distance1.values())][0]
+        child[index2] = [key for key, val in distance2.items() if val == min(distance2.values())][0]
+        child[index3] = [key for key, val in distance3.items() if val == min(distance3.values())][0]
 
         return child
-
-
 
     def show_step(self, generation, fitness):
         '''
             Plots an intermediate step for the current generation
         '''
         plot.suptitle("Clustering with Genetic Algorithms - Generation " + str(generation) + \
-    				 "\nFitness: " + str(fitness))
+                      "\nFitness: " + str(fitness))
         best = self.population[0]
 
-        plot.scatter([x for x,y in self.points],[y for x,y in self.points], c=best[0])
+        plot.scatter([x for x, y in self.points], [y for x, y in self.points], c=best[0])
 
         plot.pause(1)
-
-
 
     def show_result(self):
         '''
             Display the final result and the fitness over time
         '''
         plot.suptitle("Clustering with Genetic Algorithms - Generation " + str(self.generations) + \
-    				 "\nFitness: " + str(-self.population[0][1]))
+                      "\nFitness: " + str(-self.population[0][1]))
         best = self.population[0]
 
-        plot.scatter([x for x,y in self.points],[y for x,y in self.points], c=best[0])
+        plot.scatter([x for x, y in self.points], [y for x, y in self.points], c=best[0])
 
         plot.figure("Genetic Search - Best Fitness by Generation")
 
@@ -187,8 +168,6 @@ class GeneticSearch:
 
         plot.show()
         plot.pause(1)
-
-
 
     def run(self):
         '''
@@ -204,7 +183,7 @@ class GeneticSearch:
             parent1 = []
             parent2 = []
 
-            retain = math.ceil(self.population_size*0.05)
+            retain = math.ceil(self.population_size * 0.05)
             new_population = self.population[:retain]
             while len(new_population) < self.population_size:
 
@@ -216,7 +195,7 @@ class GeneticSearch:
                 while parent1 == parent2:
                     parent2 = random.choice(self.population)[0]
 
-                children = self.reproduce(parent1,parent2)
+                children = self.reproduce(parent1, parent2)
 
                 child1 = children[0]
                 child2 = children[1]
@@ -229,25 +208,23 @@ class GeneticSearch:
                 fitness1 = self.fitnessfcn(child1)
                 fitness2 = self.fitnessfcn(child2)
 
-                new_population.append([child1,fitness1])
-                new_population.append([child2,fitness2])
+                new_population.append([child1, fitness1])
+                new_population.append([child2, fitness2])
 
             generations = generations + 1
-            new_population.sort(key=operator.itemgetter(1),reverse=True)
+            new_population.sort(key=operator.itemgetter(1), reverse=True)
 
             self.population = new_population
 
             self.values.append(self.population[0][1])
 
             if generations % DISPLAY_RATE == 0:
-                print("Generation",generations,"Fitness",-self.population[0][1])
-                self.show_step(generations,-self.population[0][1])
-
-
+                print("Generation", generations, "Fitness", -self.population[0][1])
+                self.show_step(generations, -self.population[0][1])
 
 
 def main():
-    filename = sys.argv[1]  #TODO: Change to the filename if you do not want to enter this on the command line
+    filename = sys.argv[1]  # TODO: Change to the filename if you do not want to enter this on the command line
 
     gs = GeneticSearch(filename, 100, 10, 0.05)
     gs.run()
@@ -255,5 +232,6 @@ def main():
 
     input("Press Enter to exit...")
     plot.close()
+
 
 main()
